57cad3727aacdc1304f4feefbb67968e3b5b03d5
norbert barics <norbert.barics@gmail.com>
2017-03-22 00:16:41 +0100

    triangle task fixed

---
+++ week-02/day-02/exercise-01/src/Triangle.java
@@ -0,0 +1,21 @@
+
+public class Triangle {
+  public static void main(String[] args) {
+    int number = 5;
+    for(int i = 0; i < number +1;i++){
+      for(int j = 0; j < i; j++){
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+}
+// Write a program that reads a number from the standard input, then draws a
+// triangle like this:
+//
+// *
+// **
+// ***
+// ****
+//
+// The triangle should have as many lines as the number was
