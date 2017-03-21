fa6f372bfa394844573968adf90d10d05a8f8ebc
norbert barics <norbert.barics@gmail.com>
2017-03-21 17:44:49 +0100

    task 19 finished work like a charm

--- week-02/day-02/exercise-01/src/Task19OneInput.java
+++ week-02/day-02/exercise-01/src/Task19OneInput.java
@@ -3,6 +3,22 @@ import java.util.Scanner;
 public class Task19OneInput {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int UserInput1 = scanner.nextInt();
+    System.out.println("Pease add a number!");
+    int userInput1 = scanner.nextInt();
+    if(userInput1 < 1) {
+      System.out.println("Not enough!");
+    } else if(userInput1 == 1){
+      System.out.println("One!");
+    } else if(userInput1 == 2){
+      System.out.println("Two!");
+    } else {
+      System.out.println("A lot");
+    }
   }
 }
+
+// Write a program that reads a number form the standard input,
+// If the number is zero or smaller it should print: Not enough
+// If the number is one it should print: One
+// If the number is two it should print: Two
+// If the number is more than two it should print: A lot
