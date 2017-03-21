3635979978d173555bf7cc789501d8d3dd88ffaf
norbert barics <norbert.barics@gmail.com>
2017-03-21 23:39:18 +0100

    even numbers between 0 and 500 completed

---
+++ week-02/day-02/exercise-01/src/EvenBetween.java
@@ -0,0 +1,10 @@
+
+public class EvenBetween {
+  public static void main(String[] args) {
+    for(int i = 0; i < 500; i+=2){
+      System.out.println(i);
+    }
+  }
+}
+
+// Create a program that prints all the even numbers between 0 and 500
