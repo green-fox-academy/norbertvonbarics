5d9017e37f60fee3c93c556f6b6963727bd58514
norbert barics <norbert.barics@gmail.com>
2017-03-21 17:56:42 +0100

    comparation statement added and work like a charm

--- week-02/day-02/exercise-01/src/TwoInputPrintTheBigger.java
+++ week-02/day-02/exercise-01/src/TwoInputPrintTheBigger.java
@@ -2,8 +2,19 @@ import java.util.Scanner;
 public class TwoInputPrintTheBigger {
   public static void main(String[] args) {
     System.out.println("Please add two numbers!");
-    Scanner scanner = new scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     int userInput1 = scanner.nextInt();
-    
+    System.out.println("add one more!");
+    int userInput2 = scanner.nextInt();
+    String note = " is bigger!";
+    if (userInput1 < userInput2) {
+      System.out.println(userInput2 + note);
+    } else {
+      System.out.println(userInput1 + note);
+    }
   }
 }
+
+
+
+// Write a program that asks for two numbers and prints the bigger one
