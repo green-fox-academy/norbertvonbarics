9c5c87a9a5bfb9f5ea8ed2b5fffa2fcd994a6852
norbert barics <norbert.barics@gmail.com>
2017-03-21 23:51:39 +0100

    MultiplicationTable shell

---
+++ week-02/day-02/exercise-01/src/MultiplicationTable.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+  public static void main(String[] args) {
+    System.out.println("give me a number!");
+    Scanner scanner = new Scanner(System.in);
+    int userInput1 = scanner.nextInt();
+  }
+}
