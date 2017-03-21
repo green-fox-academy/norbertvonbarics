44b932ece123f2bdd13df1513d6db6123ea9dc25
norbert barics <norbert.barics@gmail.com>
2017-03-21 16:57:22 +0100

    add some notes to make script user friendly

--- week-02/day-02/exercise-01/src/SumAndAverage.java
+++ week-02/day-02/exercise-01/src/SumAndAverage.java
@@ -5,10 +5,15 @@ public class SumAndAverage {
     Scanner scanner = new Scanner(System.in);
     System.out.println("Please add 5 number in a row!");
     int userInput1 = scanner.nextInt();
-    System.out.println();
+    System.out.println("4 more to go");
     int userInput2 = scanner.nextInt();
+    System.out.println("yepp 3 more");
     int userInput3 = scanner.nextInt();
+    System.out.println("just 2 more");
     int userInput4 = scanner.nextInt();
+    System.out.println("annnndd 1111!");
     int userInput5 = scanner.nextInt();
+
+    
   }
 }
