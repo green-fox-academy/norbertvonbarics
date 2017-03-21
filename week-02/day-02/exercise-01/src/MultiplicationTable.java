a75c43ae7a4680f76fc46318c00da8f669741c50
norbert barics <norbert.barics@gmail.com>
2017-03-21 23:53:30 +0100

    feat(for loop) for multiplication

--- week-02/day-02/exercise-01/src/MultiplicationTable.java
+++ week-02/day-02/exercise-01/src/MultiplicationTable.java
@@ -5,5 +5,23 @@ public class MultiplicationTable {
     System.out.println("give me a number!");
     Scanner scanner = new Scanner(System.in);
     int userInput1 = scanner.nextInt();
+    for(int i = 0; i < 11; i++){
+      System.out.println(i * userInput1);
+    }
   }
 }
+// Create a program that asks for a number and prints the multiplication table with that number
+//
+// Example:
+// The number 15 should print:
+//
+// 1 * 15 = 15
+// 2 * 15 = 30
+// 3 * 15 = 45
+// 4 * 15 = 60
+// 5 * 15 = 75
+// 6 * 15 = 90
+// 7 * 15 = 105
+// 8 * 15 = 120
+// 9 * 15 = 135
+// 10 * 15 = 150
