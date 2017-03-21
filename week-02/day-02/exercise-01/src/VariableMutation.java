d21c7efd8645e7ea2eef54a06e8e14e691d3525f
norbert barics <norbert.barics@gmail.com>
2017-03-21 16:22:23 +0100

    variable mutation java til third quarter done

--- week-02/day-02/exercise-01/src/VariableMutation.java
+++ week-02/day-02/exercise-01/src/VariableMutation.java
@@ -2,7 +2,7 @@ public class VariableMutation {
   public static void main(String[] args) {
     int a = 3;
     // make it bigger by 10
-
+    a += 10;
 
     System.out.println(a);
 
@@ -11,7 +11,7 @@ public class VariableMutation {
 
     int b = 100;
     // make it smaller by 7
-
+    b -= 7;
 
     System.out.println(b);
 
@@ -20,7 +20,7 @@ public class VariableMutation {
 
     int c = 44;
     // please double c's value
-
+    c *= 2;
 
     System.out.println(c);
 
@@ -29,7 +29,7 @@ public class VariableMutation {
 
     int d = 125;
     // please divide by 5 d's value
-
+    d /= 5;
 
     System.out.println(d);
 
@@ -38,7 +38,7 @@ public class VariableMutation {
 
     int e = 8;
     // please cube of e's value
-
+    e = e * e * e ;
 
     System.out.println(e);
 
@@ -48,29 +48,43 @@ public class VariableMutation {
     int f1 = 123;
     int f2 = 345;
     // tell if f1 is bigger than f2 (print as a boolean)
-
-
+    if ( f1 > f2 ) {
+      System.out.println(true);
+    } else {
+      System.out.println(false);
+    }
 
 
 
     int g1 = 350;
     int g2 = 200;
     // tell if the double of g2 is bigger than g1 (print as a boolean)
+    if (g2*2 > g1) {
+      System.out.println(true);
+    } else {
+      System.out.println(false);
+    }
 
 
 
-
-    int h = 1357988018575474;
+    long h = 1357988018575474L;
     // tell if it has 11 as a divisor (print as a boolean)
-
-
+    if (h % 11 == 0) {
+      System.out.println(true);
+    } else {
+      System.out.println(false);
+    }
 
 
 
     int i1 = 10;
     int i2 = 3;
     // tell if i1 is higher than i2 squared and smaller than i2 cubed (print as a boolean)
-
+    if ((i1 > i2 *i2 ) && (i1 < i2 * i2 * i2)) {
+      System.out.println(true);
+    } else {
+      System.out.println("nagyon nem true");
+    }
 
 
 
