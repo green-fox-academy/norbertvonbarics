7c892b9964108330ea597500dd25437d6208511b
norbert barics <norbert.barics@gmail.com>
2017-03-21 22:55:10 +0100

    i won't cheat java finished

---
+++ week-02/day-02/exercise-01/src/IWontCheat.java
@@ -0,0 +1,12 @@
+
+public class IWontCheat {
+  public static void main(String[] args) {
+    String cheat = "I won't cheat on the exam!";
+    for(int i = 0; i < 100; i++) {
+      System.out.println(cheat);
+    }
+  }
+}
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
