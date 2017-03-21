841a32be24ed8d7e64ad152f1e7bb75a1bec20f0
norbert barics <norbert.barics@gmail.com>
2017-03-21 16:55:13 +0100

    feat(scanner feature ready to go)

---
+++ week-02/day-02/exercise-01/src/SumAndAverage.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SumAndAverage {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please add 5 number in a row!");
+    int userInput1 = scanner.nextInt();
+    System.out.println();
+    int userInput2 = scanner.nextInt();
+    int userInput3 = scanner.nextInt();
+    int userInput4 = scanner.nextInt();
+    int userInput5 = scanner.nextInt();
+  }
+}
