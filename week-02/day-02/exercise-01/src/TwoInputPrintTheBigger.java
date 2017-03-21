bf10ab0179279ed491bac4c4701d3c7f9981655c
norbert barics <norbert.barics@gmail.com>
2017-03-21 17:51:10 +0100

    compare two number input shell ready

---
+++ week-02/day-02/exercise-01/src/TwoInputPrintTheBigger.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class TwoInputPrintTheBigger {
+  public static void main(String[] args) {
+    System.out.println("Please add two numbers!");
+    Scanner scanner = new scanner(System.in);
+    int userInput1 = scanner.nextInt();
+    
+  }
+}
