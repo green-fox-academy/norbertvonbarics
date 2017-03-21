a77249a445a5ace88bcd563a122b3b5a5ed4d916
norbert barics <norbert.barics@gmail.com>
2017-03-21 17:30:04 +0100

    scanner function corrected, and added some comfort feature

--- week-02/day-02/exercise-01/src/OddOrEven.java
+++ week-02/day-02/exercise-01/src/OddOrEven.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class OddOrEven {
   public static void main(String[] args) {
-    Scanner scanner = Scanner(System.in);
+    System.out.println("Please add a number!");
+    Scanner scanner = new Scanner(System.in);
     int userInput1 = scanner.nextInt();
     if (userInput1 % 2 == 0) {
       System.out.println("Even");
