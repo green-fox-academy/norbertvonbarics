0032b89007839b6ba8811b564c0983910c2d7a29
norbert barics <norbert.barics@gmail.com>
2017-03-21 17:36:43 +0100

    task 19 shell upload

---
+++ week-02/day-02/exercise-01/src/Task19OneInput.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Task19OneInput {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int UserInput1 = scanner.nextInt();
+  }
+}
