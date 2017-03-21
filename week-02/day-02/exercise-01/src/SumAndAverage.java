c7802af7cc283ed2771a0b33ce573c1cafecf3eb
norbert barics <norbert.barics@gmail.com>
2017-03-21 17:11:48 +0100

    corrected the sum feat string, and added average function

--- week-02/day-02/exercise-01/src/SumAndAverage.java
+++ week-02/day-02/exercise-01/src/SumAndAverage.java
@@ -21,6 +21,7 @@ public class SumAndAverage {
     int userInput5 = scanner.nextInt();
 
     int sumUserInputs = userInput1 + userInput2 + userInput3 + userInput4 + userInput5;
-    System.out.println("The sum of your numbers is " + sumUserInputs + "!");
+    System.out.println("sum: "+ sumUserInputs);
+    System.out.println("Average: " (double) sumUserInputs / 5);
   }
 }
