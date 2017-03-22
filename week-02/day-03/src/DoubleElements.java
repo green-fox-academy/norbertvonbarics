42ec58e0b859c6252f12da36f739dfefae4dae07
norbert barics <norbert.barics@gmail.com>
2017-03-22 16:14:52 +0100

    double elements of an array done

---
+++ week-02/day-03/src/DoubleElements.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class DoubleElements {
+  public static void main(String[] args) {
+    int[] ag = {3, 4, 5, 6, 7};
+    for(int i = 0; i < ag.length; i++){
+      ag[i] *= 2;
+    }
+    System.out.println(Arrays.toString(ag));
+  }
+}
+
+
+//   with the following content: `[3, 4, 5, 6, 7]`
+// - Double all the values in the array
