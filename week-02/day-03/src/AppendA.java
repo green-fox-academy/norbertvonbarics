344d03efca12e1cbec506aee9bd49efc36ad3ce2
norbert barics <norbert.barics@gmail.com>
2017-03-22 16:39:59 +0100

    AppendA array commit try 2

--- week-02/day-03/src/AppendA.java
+++ week-02/day-03/src/AppendA.java
@@ -3,7 +3,7 @@ public class AppendA {
   public static void main(String[] args) {
     String[] nimals = {"kuty", "macs", "cic"};
     for(int i = 0; i < nimals.length; i++){
-      
+
     }
   }
 }
