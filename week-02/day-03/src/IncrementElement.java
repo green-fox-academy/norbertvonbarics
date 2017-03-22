5dc64084a9b88f6f2dc1f66f6309be24a738bf0b
norbert barics <norbert.barics@gmail.com>
2017-03-22 15:20:49 +0100

    increment element task ready

---
+++ week-02/day-03/src/IncrementElement.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class IncrementElement {
+  public static void main(String[] args) {
+    int[] t ={1, 2, 3, 4, 5};
+    t[2]++;
+    System.out.println(Arrays.toString(t));
+  }
+}
+
+/*
+- Create an array variable named `t`
+with the following content: `[1, 2, 3, 4, 5]`
+- Increment the third element
+- Print the third element
+*/
