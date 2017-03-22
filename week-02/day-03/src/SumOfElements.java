eaa490eaedd465b450b4c454fdbf41f1e4b010ec
norbert barics <norbert.barics@gmail.com>
2017-03-22 15:07:40 +0100

    SumOfElements task works fine

--- week-02/day-03/src/SumOfElements.java
+++ week-02/day-03/src/SumOfElements.java
@@ -1,12 +1,14 @@
-/*
+
 public class SumOfElements {
   public static void main(String[] args) {
-    int[] r = [54, 23, 66, 12];
+    int[] r = {54, 23, 66, 12};
     System.out.println(sum(r));
   }
-  public static int[] sum
+  public static int sum(int[] a){
+    return a[1] + a[2];
+  }
 }
-*/
+
 // - Create an array variable named `r`
 //   with the following content: `[54, 23, 66, 12]`
 // - Print the sum of the second and the third element
