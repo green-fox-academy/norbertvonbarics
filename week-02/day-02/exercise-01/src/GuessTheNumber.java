82a3f6dc578438fb633eccc8c92d825a262ec68a
norbert barics <norbert.barics@gmail.com>
2017-03-22 23:20:44 +0100

    GuessTheNumber infinite loop giltch resolved

--- week-02/day-02/exercise-01/src/GuessTheNumber.java
+++ week-02/day-02/exercise-01/src/GuessTheNumber.java
@@ -5,17 +5,22 @@ public class GuessTheNumber {
     Scanner myScanner = new Scanner(System.in);
     int number = 8;
     System.out.println("I guessed a number!");
-    int userInput = myScanner.nextInt();
-    while (userInput != number) {
+
+    boolean guessed = false;
+    while (!guessed) {
+      int userInput = myScanner.nextInt();
       if (userInput < number) {
         System.out.println("The stored number is higher!");
-      } else {
+      } else if (userInput > number) {
         System.out.println("The stored number is lower!");
-      }
+      } else {
       System.out.println("You found the number: " + number);
+      guessed = true;
+      }
     }
   }
 }
+
 
 // Write a program that stores a number, and the user has to figure it out.
 // The user can input guesses, after each guess the program would tell one
