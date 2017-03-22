817402d9b72941319ed050c8c2566f4729b6035b
norbert barics <norbert.barics@gmail.com>
2017-03-22 13:45:48 +0100

    DiamondShape java task done

---
+++ week-02/day-02/exercise-01/src/DiamondShape.java
@@ -0,0 +1,40 @@
+
+public class DiamondShape {
+  public static void main(String[] args) {
+    int number = 13;
+    for(int i = 0;i < number +2; i++) {
+      if(i < number/2 +1) {
+        for (int j = 0; j < number - i; j++) {
+          System.out.print(" ");
+        }
+        for (int k = 1; k <= (i * 2 - 1); k++) {
+          System.out.print("*");
+        }
+      } else{
+        for (int k = 1; k <= (i -1 ); k++) {
+          System.out.print(" ");
+        /*  System.out.print(k);*/
+        }
+        for (int j = 0; j < (number - i) *2 +1; j++) {
+          System.out.print("*");
+         /* System.out.print(j);*/
+        }
+
+      } System.out.println();
+    }
+  }
+}
+
+// Write a program that reads a number from the standard input, then draws a
+// diamond like this:
+//
+//
+//    *
+//   ***
+//  *****
+// *******
+//  *****
+//   ***
+//    *
+//
+// The diamond should have as many lines as the number was
