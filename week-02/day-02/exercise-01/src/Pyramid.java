8bf8850f509dea83549251b646663aa55e5720e9
norbert barics <norbert.barics@gmail.com>
2017-03-22 13:23:09 +0100

    pyramid task ready

--- week-02/day-02/exercise-01/src/Pyramid.java
+++ week-02/day-02/exercise-01/src/Pyramid.java
@@ -2,19 +2,18 @@
 public class Pyramid {
   public static void main(String[] args) {
     int number = 11;
-    for (int i = 0; i < number; i++) {
-      int spaceCount = (number - i);
-      for(int j = 0; j< number; j++) {
-        if(j < spaceCount || j >= (number - spaceCount)) {
-          System.out.print(" ");
-        } else {
-          System.out.print("*");
-        }
+    for(int i = 0;i < number; i++) {
+      for(int j = 0; j < number-i; j++) {
+        System.out.print(" ");
+      }
+      for(int k = 1; k <= (i * 2 -1); k++) {
+        System.out.print("*");
       }
       System.out.println();
     }
+    }
   }
-}
+
 // Write a program that reads a number from the standard input, then draws a
 // pyramid like this:
 //
