c2b116deef0d08109384ed579e3458e342bce661
norbert barics <norbert.barics@gmail.com>
2017-03-22 11:41:40 +0100

    SumAll return problem solved

---
+++ week-02/day-03/src/SumAll.java
@@ -0,0 +1,23 @@
+
+public class SumAll {
+  public static void main(String[] args) {
+    int num1 = 5;
+    System.out.println(sum(num1));
+  }
+  public static int sum(int a){
+    int x = 0;
+    if(a == 1){
+      x = 1;
+    } else if(a > 1){
+      for(int i =0; i < a ; i++){
+        x+=i;
+      }
+    } return x;
+  }
+}
+
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Write a function called `sum` that sum all the numbers
+//   until the given parameter
