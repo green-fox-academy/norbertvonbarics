53b78a9a0d95436a043dd1898d977e1c5c33f4f5
norbert barics <norbert.barics@gmail.com>
2017-03-22 16:35:58 +0100

    AppendA shell, and array ready

---
+++ week-02/day-03/src/AppendA.java
@@ -0,0 +1,13 @@
+
+public class AppendA {
+  public static void main(String[] args) {
+    String[] nimals = {"kuty", "macs", "cic"};
+    for(int i = 0; i < nimals.length; i++){
+      
+    }
+  }
+}
+
+// - Create an array variable named `nimals`
+//   with the following content: `["kuty", "macs", "cic"]`
+// - Add all elements an `"a"` at the end
