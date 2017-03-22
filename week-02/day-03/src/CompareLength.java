06fb41870cd671673ce4914c57d9913c4727c866
norbert barics <norbert.barics@gmail.com>
2017-03-22 14:57:47 +0100

    CompareLength task completed + SumOfElements shell

---
+++ week-02/day-03/src/CompareLength.java
@@ -0,0 +1,23 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class CompareLength {
+  public static void main(String[] args) {
+    int[] p1 = {1, 2, 3};
+    int[] p2 = {4, 5};
+    System.out.println(Arrays.toString(compare(p1, p2)));
+  }
+  public static int[] compare(int[]a, int[]b ) {
+    if(a.length > b.length){
+      return a;
+    } else {
+      return b;
+    }
+  }
+}
+
+// - Create an array variable named `p1`
+//   with the following content: `[1, 2, 3]`
+// - Create an array variable named `p2`
+//   with the following content: `[4, 5]`
+// - Print if `p2` has more elements than `p1`
---
+++ week-02/day-03/src/SumOfElements.java
@@ -0,0 +1,12 @@
+/*
+public class SumOfElements {
+  public static void main(String[] args) {
+    int[] r = [54, 23, 66, 12];
+    System.out.println(sum(r));
+  }
+  public static int[] sum
+}
+*/
+// - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
