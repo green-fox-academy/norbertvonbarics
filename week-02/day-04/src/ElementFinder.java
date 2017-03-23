4cbc35c078e5690dc3a1f88e42c08f623af6fca6
norbert barics <norbert.barics@gmail.com>
2017-03-23 14:35:59 +0100

    best solution of the search function so far

--- week-02/day-04/src/ElementFinder.java
+++ week-02/day-04/src/ElementFinder.java
@@ -3,15 +3,14 @@ import java.util.*;
 public class ElementFinder {
   public static void main(String... args) {
     ArrayList<Integer> arrayList = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
-    for (int i : arrayList) {
-     if (i == 7) {
-       System.out.println("Hooreray");
-     } else {
-       System.out.println("Nooooo");
-     }
+      if (arrayList.contains(7)) {
+        System.out.println("Hoorray");
+      } else {
+        System.out.println("Nooooo");
+      }
     }
   }
-}
+
 
 // Check if the arrayList contains "7" if it contains print "Hoorray" otherwise print "Noooooo"
 // Do this again with a different solution using different list functions!
