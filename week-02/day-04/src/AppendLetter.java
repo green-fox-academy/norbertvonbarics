f33472399ccbf0c996ef51da685ac27fc5aa5ac8
norbert barics <norbert.barics@gmail.com>
2017-03-23 13:56:40 +0100

    AppendLetter shell

---
+++ week-02/day-04/src/AppendLetter.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class AppendLetter{
+  public static void main(String... args){
+    ArrayList<String> far = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
+    // Add "a" to every string in the far list.
+    for (int i = 0; i < far.size(); i++) {
+      far.get(i).c;
+    }
+    System.out.println(far);
+  }
+}
