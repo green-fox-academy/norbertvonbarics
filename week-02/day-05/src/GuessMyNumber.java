db160c8e423a26db7a9e8451f32cebe02ecc6b0e
norbert barics <norbert.barics@gmail.com>
2017-03-24 10:13:36 +0100

    feedbacks to user added

--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -7,12 +7,17 @@ public class GuessMyNumber {
     System.out.println(myInt);
     Scanner myScanner = new Scanner(System.in);
 
-    for (int i = 0;i < 3; i++) {
+    for (int i = 3;i > 0; i--) {
       int userInput = myScanner.nextInt();
-      if ((i == 2) && (myInt !=  userInput)) {
-        System.out.println("YOU LOSE!");
+      if ((i == 1) && (myInt !=  userInput)) {
+        System.out.println("YOU LOSE! MY number was" + " " + (int) random);
       } else if (myInt != userInput) {
-        System.out.println("Guess angain!");
+          if (myInt > userInput){
+            System.out.println("Your number is to low!");
+          } else {
+            System.out.println("Your number is too high!");
+          }
+        System.out.println("Guess angain!" + " " + "remain guesses: " + (i-1));
       } else if (myInt == userInput) {
         System.out.println("YOU WIN!");
       }
