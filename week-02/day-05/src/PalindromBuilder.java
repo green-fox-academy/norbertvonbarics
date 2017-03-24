06d8c8d8ccd4243802cdc5d3c84e6a77bbf85a07
norbert barics <norbert.barics@gmail.com>
2017-03-24 08:28:52 +0100

    userinput ready both in searcher and builder

--- week-02/day-05/src/PalindromBuilder.java
+++ week-02/day-05/src/PalindromBuilder.java
@@ -1,8 +1,13 @@
 import java.util.Scanner;
 
-public class PalindromBuilder {
+public class PalindromSearcher {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
-    String userInput = myScanner.next();
+    String input = myScanner.nextLine();
+    System.out.println(input);
+
+    for (int i = 0; i < input.length(); i++) {
+      System.out.println();
+    }
   }
 }
--- week-02/day-05/src/PalindromSearcher.java
+++ week-02/day-05/src/PalindromSearcher.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 public class PalindromSearcher {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
-    String userInput = myScanner.next();
+    String input = myScanner.nextLine();
+    System.out.println(input);
+
+    for (int i = 0; i < input.length(); i++) {
+      System.out.println();
+    }
   }
 }
