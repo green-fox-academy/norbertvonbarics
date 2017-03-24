301b361858a4f55e04182cc6ea34b0cec934e2be
norbert barics <norbert.barics@gmail.com>
2017-03-24 10:27:35 +0100

    range feature added

--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -2,22 +2,27 @@ import java.util.Scanner;
 
 public class GuessMyNumber {
   public static void main(String[] args) {
-    double random = Math.floor((Math.random() * 100) + 1);
-    int myInt = (int) random;
-    System.out.println(myInt);
     Scanner myScanner = new Scanner(System.in);
 
-    for (int i = 3;i > 0; i--) {
+    System.out.println("Please add the range of the number I have to think!");
+    int range = myScanner.nextInt();
+    System.out.println("Okay, my number is between 0 and" + " " + range);
+
+    double random = Math.floor((Math.random() * range) + 1);
+    int myInt = (int) random;
+    System.out.println(myInt);
+
+    for (int i = 3; i > 0; i--) {
       int userInput = myScanner.nextInt();
-      if ((i == 1) && (myInt !=  userInput)) {
+      if ((i == 1) && (myInt != userInput)) {
         System.out.println("YOU LOSE! MY number was" + " " + (int) random);
       } else if (myInt != userInput) {
-          if (myInt > userInput){
-            System.out.println("Your number is to low!");
-          } else {
-            System.out.println("Your number is too high!");
-          }
-        System.out.println("Guess angain!" + " " + "remain guesses: " + (i-1));
+        if (myInt > userInput) {
+          System.out.println("Your number is to low!");
+        } else {
+          System.out.println("Your number is too high!");
+        }
+        System.out.println("Guess again!" + " " + "remain guesses: " + (i - 1));
       } else if (myInt == userInput) {
         System.out.println("YOU WIN!");
       }
