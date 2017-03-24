e2474a96c5d8c7d42675088c7d6ac1fa18217380
norbert barics <norbert.barics@gmail.com>
2017-03-24 09:56:06 +0100

    guess the number guess function added

--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -1,13 +1,21 @@
 import java.util.Scanner;
-import java.math.*;
 
 public class GuessMyNumber {
   public static void main(String[] args) {
+    double random = Math.floor((Math.random() * 100) + 1);
+    int myInt = (int) random;
+    System.out.println(myInt);
     Scanner myScanner = new Scanner(System.in);
-    for (int i = 0; i < 100; i++) {
-      double random = Math.floor((Math.random() * 100)+1);
-      System.out.println(random);
-    }
 
+    for (int i = 0;i < 3; i++) {
+      int userInput = myScanner.nextInt();
+      if ((i == 2) && (myInt !=  userInput)) {
+        System.out.println("YOU LOSE!");
+      } else if (myInt != userInput) {
+        System.out.println("Guess angain!");
+      } else if (myInt == userInput) {
+        System.out.println("YOU WIN!");
+      }
+    }
   }
 }
