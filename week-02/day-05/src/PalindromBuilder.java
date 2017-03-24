06bfd9916c7440d2a36536a10780dbb080c862bb
norbert barics <norbert.barics@gmail.com>
2017-03-24 08:19:48 +0100

    palindrom searcher and builder shell

---
+++ week-02/day-05/src/PalindromBuilder.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class PalindromBuilder {
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    String userInput = myScanner.next();
+  }
+}
---
+++ week-02/day-05/src/PalindromSearcher.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class PalindromSearcher {
+  public static void main(String[] args) {
+    Scanner myScanner = new Scanner(System.in);
+    String userInput = myScanner.next();
+  }
+}
