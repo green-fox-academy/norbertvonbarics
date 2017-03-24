2cc209e4e8b2a366a83a94a6eb998d95ff076ea1
norbert barics <norbert.barics@gmail.com>
2017-03-24 09:25:49 +0100

    random number generator working

--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
 import java.math.*;
+
 public class GuessMyNumber {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
-    for (int i = 0; i < 15;i++){
-    double random = Math.random() * ((100) + 1);
-    System.out.println(random);}
+    for (int i = 0; i < 100; i++) {
+      double random = Math.floor((Math.random() * 100)+1);
+      System.out.println(random);
+    }
+
   }
 }
