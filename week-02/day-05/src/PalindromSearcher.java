6d50b9927da07b7833575e5946b0fa4e28a72ae5
norbert barics <norbert.barics@gmail.com>
2017-03-25 23:22:18 +0100

    palindrom searcher phase 1

--- week-02/day-05/src/PalindromSearcher.java
+++ week-02/day-05/src/PalindromSearcher.java
@@ -1,21 +1,32 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PalindromSearcher {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     String input = myScanner.nextLine();
-    ArrayList<String> palindromList = new ArrayList <String>;
-
-
-    isPalindrom(input);
+    ArrayList<String> palindromList = new ArrayList<String>();
     System.out.println(isPalindrom(input));
+    System.out.println(Arrays.toString(palindromList.toArray()));
+
+    for (int i = 2; i < input.length(); i++) {
+      int counter = i;
+      for (int j = 0; j < input.length() - i; j++) {
+        if (input.charAt(counter) == input.charAt(j)) {
+          String maybeP = input.substring(j, counter + 1);
+          if (isPalindrom(maybeP)) {
+            palindromList.add(maybeP);
+          }
+        }
+        counter++;
+      }
+    }
   }
 
-  public static boolean isPalindrom (String verse) {
+  private static boolean isPalindrom(String verse) {
     StringBuilder reverser = new StringBuilder(verse);
     String reversed = reverser.reverse().toString();
-    return verse.equals(reversed);
+    return (verse.equals(reversed));
   }
-
 }
