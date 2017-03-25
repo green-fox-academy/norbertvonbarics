8963e77d7dcfad891c4342f4b0936a27b34c368c
norbert barics <norbert.barics@gmail.com>
2017-03-25 13:38:11 +0100

    palindromSearcher's is palindrom function ready to go

--- week-02/day-05/src/PalindromSearcher.java
+++ week-02/day-05/src/PalindromSearcher.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PalindromSearcher {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     String input = myScanner.nextLine();
+    ArrayList<String> palindromList = new ArrayList <String>;
 
-    String reversed = "";
-    for(int i = input.length()-1; i > -1; i--){
-      reversed += input.charAt(i);
-    }
-    String palindrom = input.concat(reversed);
-    System.out.println(palindrom);
+
+    isPalindrom(input);
+    System.out.println(isPalindrom(input));
   }
+
+  public static boolean isPalindrom (String verse) {
+    StringBuilder reverser = new StringBuilder(verse);
+    String reversed = reverser.reverse().toString();
+    return verse.equals(reversed);
+  }
+
 }
