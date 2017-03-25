837d53fd9269aa0636d7b3e002dc4203d9748f53
norbert barics <norbert.barics@gmail.com>
2017-03-25 13:20:14 +0100

    palindromBuilder upgraded with a builder function

--- week-02/day-05/src/PalindromBuilder.java
+++ week-02/day-05/src/PalindromBuilder.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class PalindromBuilder {
   public static void main(String[] args) {
-    Scanner newScanner = new Scanner(System.in);
-    String input = newScanner.nextLine();
-    String[] palBlock = input.split(" ");
+    Scanner myScanner = new Scanner(System.in);
+    String input = myScanner.nextLine();
 
+    palindromeSearcher(input);
+    System.out.println(input);
+    System.out.println(palindromeSearcher(input));
+  }
+  public static String palindromeSearcher(String a) {
     String reversed = "";
-    for(int i = input.length()-1; i > -1; i--){
-      reversed += input.charAt(i);
+    for(int i = a.length()-1; i > -1; i--) {
+      reversed += a.charAt(i);
     }
-    String palindrom = input.concat(reversed);
-    System.out.println(palindrom);
+    String finalString = a.concat(reversed);
+    return finalString;
   }
 }
