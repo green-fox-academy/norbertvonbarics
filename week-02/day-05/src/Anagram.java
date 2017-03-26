5fa766015533b8f1dcc523309289796302ce04da
norbert barics <norbert.barics@gmail.com>
2017-03-26 12:16:35 +0200

    anagram function with arrays ready and works like a charm

--- week-02/day-05/src/Anagram.java
+++ week-02/day-05/src/Anagram.java
@@ -6,12 +6,17 @@ public class Anagram {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
     System.out.println("");
+    System.out.println("Please add word one!");
     String userInput1 = myScanner.nextLine();
+    System.out.println("Please add word two!");
     String userInput2 = myScanner.nextLine();
-    System.out.println(userInput1);
-    System.out.println(userInput2);
+    System.out.println("Word one is: " + userInput1);
+    System.out.println("Word two is: " + userInput2);
     System.out.println(isAnagram(userInput1, userInput2));
-
+    if (isAnagram(userInput1, userInput2)) {
+      System.out.println("Anagram found!");
+    } else {
+      System.out.println("Better luck next time!");}
   }
 
   public static boolean isAnagram(String user1, String user2) {
@@ -19,8 +24,8 @@ public class Anagram {
     String[] userArray2 = user2.split("");
     Arrays.sort(userArray1);
     Arrays.sort(userArray2);
-    System.out.println(Arrays.toString(userArray1));
-    System.out.println(Arrays.toString(userArray2));
+    user1 = Arrays.toString(userArray1);
+    user2 = Arrays.toString(userArray2);
     return (user1.equals(user2));
   }
 }
