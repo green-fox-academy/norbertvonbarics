7d476926385a5ae2cf640039cb29d67aac5d5ee2
norbert barics <norbert.barics@gmail.com>
2017-03-26 12:03:34 +0200

    anagram with reverse method working

--- week-02/day-05/src/Anagram.java
+++ week-02/day-05/src/Anagram.java
@@ -3,11 +3,18 @@ import java.util.Scanner;
 public class Anagram {
   public static void main(String[] args) {
     Scanner myScanner = new Scanner(System.in);
+    System.out.println("");
+    String userInput1 = myScanner.nextLine();
+    String userInput2 = myScanner.nextLine();
+    System.out.println(userInput1);
+    System.out.println(userInput2);
+    System.out.println(isAnagram(userInput1, userInput2));
+
   }
 
-  public static boolean isAnagram(String possibleAnagram) {
-    StringBuilder testAna = new StringBuilder(possibleAnagram);
-    String reversed = testAna.reverse().toString();
-    
+  public static boolean isAnagram(String user1, String user2) {
+    StringBuilder reverser = new StringBuilder(user2);
+    String reversedUser2 = reverser.reverse().toString();
+    return (user1.equals(reversedUser2));
   }
 }
