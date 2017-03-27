56f45e56402a45a7c6b5e6712d6b7a9898c2c452
norbert barics <norbert.barics@gmail.com>
2017-03-27 20:22:11 +0200

    decoder method feat added

--- week-03/day-01/src/EncodedList.java
+++ week-03/day-01/src/EncodedList.java
@@ -6,10 +6,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EncodedList {
-
   public static void main(String[] args) {
     System.out.println(fileRead());
+    System.out.println(decoder(fileRead()));
   }
+
+  public static List<String> decoder(List<String> text) {
+    ArrayList<String> decodedMessage = new ArrayList<>();
+    for (String line : text) {
+      StringBuilder decoder = new StringBuilder();
+      for (int i = 0; i < line.length(); i++) {
+        int charValue = line.charAt(i);
+        if (!Character.isWhitespace(line.charAt(i))) {
+          decoder.append(String.valueOf((char) (charValue - 1)));
+        } else {
+          decoder.append(line.charAt(i));
+        }
+      }
+      decodedMessage.add(decoder.toString());
+    }
+    return decodedMessage;
+  }
+
+
 
   public static List<String> fileRead() {
     List<String> lines;
