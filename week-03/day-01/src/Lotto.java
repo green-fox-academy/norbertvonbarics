6820e6fb2f05cb8c483bd4266b68610dc2418cd4
norbert barics <norbert.barics@gmail.com>
2017-03-27 13:28:46 +0200

    numbers arraylist ready to search

--- week-03/day-01/src/Lotto.java
+++ week-03/day-01/src/Lotto.java
@@ -11,27 +11,29 @@ import java.util.List;
 
 public class Lotto {
   public static void main(String[] args) {
-     Path lotto = Paths.get("otos.txt");
-     try {
-       System.out.println(Files.exists(lotto));
-       System.out.println(Files.isReadable(lotto));
-       List<String> text = Files.readAllLines(lotto);
-       ArrayList<String> splitted = new ArrayList<>();
-       System.out.println(text);
-       for (int i = 0; i < text.size(); i++) {
-         splitted.add(Arrays.toString(text.get(i).split(";")));
-       }
-       System.out.println();
-    }catch (IOException ex) {
+    Path lotto = Paths.get("otos.txt");
+    try {
+      List<String> text = Files.readAllLines(lotto);
+      ArrayList<String> numbers = new ArrayList<>();
+      String[] separate = {};
+      for (String line : text) {
+        separate = line.split(";");
+        for (int i = separate.length - 1; i > separate.length - 6; i--) {
+          numbers.add(separate[i]);
+        }
+      }
+      System.out.println(numbers);
+    } catch (IOException ex) {
       System.out.println("Catch executed!");
     }
-    numberChecker(splitted);
-    public static ArrayList numberChecker (ArrayList a){
-
-    }
-
-
+    /*numberChecker(splitted)
   }
+  public static ArrayList numberChecker(ArrayList a) {
+    System.out.println(a);
+
+    return */
+  }
+
 }
 
 // Create a method that find the 5 most common lotto numbers assets/lotto.csv
