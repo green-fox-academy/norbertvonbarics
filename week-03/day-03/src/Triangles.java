e6fbc99956449df92e1c8e873de177a38cb04f8c
norbert barics <norbert.barics@gmail.com>
2017-03-29 17:40:46 +0200

    Triangles task shell

--- week-03/day-03/src/Triangles.java
+++ week-03/day-03/src/Triangles.java
@@ -4,7 +4,7 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class StarryNight {
+public class Triangles {
 
   public static void mainDraw(Graphics graphics) {
     // draw the night sky:
@@ -12,24 +12,24 @@ public class StarryNight {
     // - The stars can be small squares
     // - The stars should have random positions on the canvas
     // - The stars should have random color (some shade of grey)
-    int randomNumberOfStars = (int) Math.ceil(Math.random() * 1000);
-    Starry(graphics, randomNumberOfStars );
-  }
+    int xpoints[] = {150, 138, 162};
+    int ypoints[] = {10, 20, 20};
+    int npoints = 3;
 
+    graphics.drawPolygon(xpoints, ypoints, npoints);
+  }
+/*
   public static void Starry(Graphics dots, int numberOfStars) {
     dots.setColor(Color.black);
     dots.fillRect(0,0,300,300);
 
     for (int i = 0; i < numberOfStars; i++) {
       int randomColor = 115 + (int) Math.ceil(Math.random() * 115);
-      Color myColor = new Color(randomColor, randomColor, randomColor);
-      dots.setColor(myColor);
-      int randomNumberOne = (int) Math.ceil(Math.random() * 300);
-      int randomNumberTwo = (int) Math.ceil(Math.random() * 300);
 
-      dots.drawLine(randomNumberOne, randomNumberTwo, randomNumberOne, randomNumberTwo);
+
+      dots.drawLine();
     }
-  }
+  }*/
 
   //    Don't touch the code below
   public static void main(String[] args) {
