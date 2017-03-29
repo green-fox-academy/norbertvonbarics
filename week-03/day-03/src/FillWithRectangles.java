1e8860a64f59b7b527d425d122dfbd5fdeb15806
norbert barics <norbert.barics@gmail.com>
2017-03-29 10:16:28 +0200

    colored box task corrected

--- week-03/day-03/src/FillWithRectangles.java
+++ week-03/day-03/src/FillWithRectangles.java
@@ -4,17 +4,24 @@ import java.awt.*;
 
 import static javax.swing.JFrame.EXIT_ON_CLOSE;
 
-public class ColoredBox {
+public class FillWithRectangles {
 
-  public static void mainDraw(Graphics graphics){
-    // draw a box that has different colored lines on each edge.
-    graphics.drawLine(0,0,300,0);
+  public static void mainDraw(Graphics graphics) {
+    // draw four different size and color rectangles.
+    int random1 = 0;
+    int random2 = 0;
+
+    for (int i = 0; i < 4; i++){
+      random1 = (int) Math.ceil(Math.random() * 200);
+      random2 = (int) Math.ceil(Math.random()* 100);
+    }
+
     graphics.setColor(Color.red);
-    graphics.drawLine(300,0, 300,300);
-    graphics.setColor(Color.blue);
-    graphics.drawLine(300,300, 0,300);
-    graphics.setColor(Color.yellow);
-    graphics.drawLine(0,300, 0,0);
+    graphics.fillRect(0,0, random1, random2);
+    graphics.fillRect(200,0, random1, random2);
+    graphics.fillRect(200,200, random1, random2);
+    graphics.fillRect(0,200, random1, random2);
+
   }
 
   //    Don't touch the code below
@@ -26,7 +33,8 @@ public class ColoredBox {
     jFrame.setLocationRelativeTo(null);
     jFrame.setVisible(true);
   }
-  static class ImagePanel extends JPanel{
+
+  static class ImagePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics graphics) {
       super.paintComponent(graphics);
