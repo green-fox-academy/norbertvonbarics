6fc307fdf5dd591899f95cc8da10301da81f76d1
norbert barics <norbert.barics@gmail.com>
2017-03-30 10:46:36 +0200

    Strings removed comments

--- week-03/day-04/src/Strings.java
+++ week-03/day-04/src/Strings.java
@@ -12,7 +12,7 @@ public class Strings {
     if (text.charAt(0) == from) {
       text = to + text.substring(1);
     }
-      return text.charAt(0) + xToY(text.substring(1, text.length()), from, to);
+    return text.charAt(0) + xToY(text.substring(1, text.length()), from, to);
 
   }
 }
@@ -20,16 +20,3 @@ public class Strings {
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
 
-/*
-
-    replace(s, from, to);
-
-    if (s.length() < 1)
-        return s;
-    if (s.charAt(0) == from) {
-        s = to + s.substring(1);
-    }
-    System.out.println(s);
-return s.charAt(0) + replace(s.substring(1, s.length()), from, to);
-}
- */
