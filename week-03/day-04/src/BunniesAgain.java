8b82ef238306f164d6933eb40ed43176dad15cb3
norbert barics <norbert.barics@gmail.com>
2017-03-30 10:26:30 +0200

    BunniesAgain's ear counter, 2 or 3 ears with recursive function

---
+++ week-03/day-04/src/BunniesAgain.java
@@ -0,0 +1,20 @@
+
+public class BunniesAgain {
+  public static void main(String[] args) {
+    System.out.println(bunny(4));
+  }
+  public static int bunny (int num) {
+    if ( num == 0) {
+      return 0;
+    } else if (num % 2 ==  0) {
+      return 3 + bunny(num -1);
+    } else {
+      return 2 + bunny(num -1);
+    }
+  }
+}
+
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies
+// (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say
+// have 3 ears, because they each have a raised foot. Recursively return the
+// number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
