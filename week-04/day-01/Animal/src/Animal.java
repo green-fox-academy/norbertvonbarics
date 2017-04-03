31e772b1073628cf359a02826d03db46faa541b2
norbert barics <norbert.barics@gmail.com>
2017-04-03 13:43:22 +0200

    animal task with void play method

---
+++ week-04/day-01/Animal/src/Animal.java
@@ -0,0 +1,52 @@
+/*
+Every animal has a hunger value, which is a whole number
+Every animal has a thirst value, which is a whole number
+when creating a new animal object these values are created with the default 50 value
+Every animal can eat() which decreases their hunger by one
+Every animal can drink() which decreases their thirst by one
+Every animal can play() which increases both by one
+ */
+public class Animal {
+  private int hunger;
+  private int thirst;
+  private String name;
+
+  public Animal() {
+    this("el chupacabra", 50, 50);
+  }
+
+  public Animal(String name, int hunger, int thirst) {
+    this.name = name;
+    this.hunger = hunger;
+    this.thirst = thirst;
+  }
+
+  public int eat() {
+    return hunger--;
+  }
+
+  public int drink() {
+    return thirst--;
+  }
+
+  void play() {
+    hunger++;
+    thirst++;
+  }
+
+  public static void main(String[] args) {
+    Animal zebra = new Animal();
+
+    System.out.println(zebra.hunger);
+    zebra.eat();
+    System.out.println(zebra.hunger);
+    System.out.println(zebra.thirst);
+    zebra.drink();
+    System.out.println(zebra.thirst);
+    for (int i = 0; i < 5; i++) {
+      zebra.play();
+    }
+    System.out.println(zebra.thirst);
+    System.out.println(zebra.hunger);
+  }
+}
