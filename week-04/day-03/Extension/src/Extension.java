349930a056f9f56f696778385d0c59cf9b321c11
norbert barics <norbert.barics@gmail.com>
2017-04-05 14:03:10 +0200

    extension median test passed

---
+++ week-04/day-03/Extension/src/Extension.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Extension {
+  int add(int a, int b) {
+    return a + b;
+  }
+
+  int maxOfThree(int a, int b, int c) {
+    if ((a > b) && (a > c)) {
+      return a;
+    } else if ((b > c) && (b > a)) {
+      return b;
+    } else return c;
+  }
+
+  int median(List<Integer> pool) {
+    int sum = 0;
+    for (int i = 0;i < pool.size(); i++){
+      sum += pool.get(i);
+    }
+    return sum / pool.size();
+  }
+
+  boolean isVowel(char c) {
+    return Arrays.asList('a', 'u', 'o', 'e', 'i').contains(c);
+  }
+
+  String translate(String hungarian) {
+    String teve = hungarian;
+    int length = teve.length();
+    for (int i = 0; i < length; i++) {
+      char c = teve.charAt(i);
+      if (isVowel(c)) {
+        teve = String.join(c + "v" + c, teve.split("" + c));
+        i += 2;
+        length += 2;
+      }
+    }
+    return teve;
+  }
+}
