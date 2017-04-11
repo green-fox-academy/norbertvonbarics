df88b7c9a1b498c2edb1ff22d2796b2098b32af1
norbert barics <norbert.barics@gmail.com>
2017-04-11 12:40:10 +0200

    hero facing the way he moves

--- week-04/day-03/CowsAndBulls/src/CowsAndBulls.java
+++ week-04/day-03/CowsAndBulls/src/CowsAndBulls.java
@@ -1,10 +1,46 @@
+import java.util.Scanner;
 
 public class CowsAndBulls {
   public static void main(String[] args) {
-for(int i = 0; i < 1000; i++) {
-    int number = (int) Math.ceil(Math.random()*8999);
-    System.out.println(number);}
 
+    checker(randomNumber(), myScanner());
   }
 
+  public static void checker(String randomNumber, String userNumber) {
+    for (int i = 0; i < 4;i++) {
+      if ((int)randomNumber.charAt(i) == (int)userNumber.charAt(i)) {
+        System.out.println("COW!!");
+      } else if ((int)randomNumber.charAt(i) =)
+    }
+  }
+
+  public static String myScanner() {
+    Scanner myScanner = new Scanner(System.in);
+
+    String userInput4Digit = "";
+    for (int i = 4; i > 0; i--) {
+      System.out.println("I need " + i + " digits!");
+      int userInput = myScanner.nextInt();
+      userInput4Digit += userInput;
+    }
+    System.out.println(userInput4Digit);
+    return userInput4Digit;
+  }
+
+  public static String randomNumber() {
+    //four digits
+    int digit1 = (int) Math.floor(Math.random() * 10);
+    int digit2 = (int) Math.floor(Math.random() * 10);
+    int digit3 = (int) Math.floor(Math.random() * 10);
+    int digit4 = (int) Math.floor(Math.random() * 10);
+
+    StringBuilder sb = new StringBuilder();
+    sb.append(digit1);
+    sb.append(digit2);
+    sb.append(digit3);
+    sb.append(digit4);
+    String fourDigitNumber = sb.toString();
+    System.out.println(fourDigitNumber);
+    return fourDigitNumber;
+  }
 }
--- week-05/day-02/src/Board.java
+++ week-05/day-02/src/Board.java
@@ -4,7 +4,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Board extends JComponent implements KeyListener {
-
+  int angle;
   int testBoxX;
   int testBoxY;
 
@@ -23,19 +23,54 @@ public class Board extends JComponent implements KeyListener {
 
     // here you have a 720x720 canvas
     // you can create and draw an image using the class below e.g.
+
+    int[][] multi = new int[][]{
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 1, 0, 1, 1, 0, 1, 1, 1, 0},
+            {1, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 1, 0, 1, 1, 0, 1, 0},
+            {1, 1, 1, 1, 0, 1, 1, 0, 1, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 1, 1, 1, 1, 1, 0, 1, 1, 1},
+            {0, 0, 1, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 1, 1, 1, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    };
+
     int posX = 0;
     int posY = 0;
-    for (int i = 0; i < 10; i++) {
-      for (int j = 0; j < 10; j++) {
-        PositionedImage image = new PositionedImage("./assets/floor.png", posX, posY);
-        image.draw(graphics);
-        posX = j * 72;
-      }
-      posY = i * 72;
-    }
-    graphics.fillRect(testBoxX, testBoxY, 72, 72);
 
-    PositionedImage image = new PositionedImage("./assets/wall.png", posX, posY);
+    for (int i = 0; i < multi.length; i++) {
+      for (int j = 0; j < multi.length; j++) {
+        if (multi[i][j] == 1) {
+          posX = i * 72;
+          posY = j * 72;
+          PositionedImage wall = new PositionedImage("./assets/wall.png", posX, posY);
+          wall.draw(graphics);
+        } else {
+          posX = i * 72;
+          posY = j * 72;
+          PositionedImage image = new PositionedImage("./assets/floor.png", posX, posY);
+          image.draw(graphics);
+        }
+      }
+    }
+    PositionedImage startHero = new PositionedImage("./assets/hero-down.png", testBoxX, testBoxY);
+    startHero.draw(graphics);
+
+    if (angle == 1) {
+      PositionedImage hero = new PositionedImage("./assets/hero-down.png", testBoxX, testBoxY);
+      hero.draw(graphics);
+    } else if (angle == 2) {
+      PositionedImage hero = new PositionedImage("./assets/hero-up.png", testBoxX, testBoxY);
+      hero.draw(graphics);
+    } else if (angle == 3) {
+      PositionedImage hero = new PositionedImage("./assets/hero-right.png", testBoxX, testBoxY);
+      hero.draw(graphics);
+    } else if (angle == 4) {
+      PositionedImage hero = new PositionedImage("./assets/hero-left.png", testBoxX, testBoxY);
+      hero.draw(graphics);
+    }
   }
 
   // To be a KeyListener the class needs to have these 3 methods in it
@@ -53,14 +88,20 @@ public class Board extends JComponent implements KeyListener {
   @Override
   public void keyReleased(KeyEvent e) {
     // When the up or down keys hit, we change the position of our box
-    if (e.getKeyCode() == KeyEvent.VK_UP) {
-      testBoxY -= 72;
-    } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
-      testBoxY += 72;
-    } else if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
-      testBoxX += 72;
-    } else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
-      testBoxX -= 72;
+    if (testBoxX >= 0 && testBoxX <= 648) {
+      if (e.getKeyCode() == KeyEvent.VK_UP) {
+        testBoxY -= 72;
+        angle = 2;
+      } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+        testBoxY += 72;
+        angle = 1;
+      } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+        testBoxX += 72;
+        angle = 3;
+      } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+        testBoxX -= 72;
+        angle = 4;
+      }
     }
     // and redraw to have a new picture with the new coordinates
     repaint();
