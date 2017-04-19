2b866391ec2e48ccb8f4d8798e75685c931c80a1
norbert barics <norbert.barics@gmail.com>
2017-04-19 11:14:54 +0200

    blackjack again... shell added

---
+++ week-06/day-03/TwentyPlusOne/src/Card.java
@@ -0,0 +1,6 @@
+
+public class Card {
+  public enum Color {RED, BLACK}
+  public enum Suit {HEARTS, CLUBS, SPADES, DIAMONDS}
+  public enum Rank {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE}
+}
