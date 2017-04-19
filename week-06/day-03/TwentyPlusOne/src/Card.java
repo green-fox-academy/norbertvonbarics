fff1767c826d0d12d86a969287ff7ac7e86a78c5
norbert barics <norbert.barics@gmail.com>
2017-04-19 11:22:12 +0200

    Values, and getValue method added to cards

--- week-06/day-03/TwentyPlusOne/src/Card.java
+++ week-06/day-03/TwentyPlusOne/src/Card.java
@@ -1,6 +1,21 @@
 
 public class Card {
   public enum Color {RED, BLACK}
+
   public enum Suit {HEARTS, CLUBS, SPADES, DIAMONDS}
-  public enum Rank {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE}
+
+  public enum Rank {
+    TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(13), ACE(14);
+
+    private final int value;
+
+    Rank(int value) {
+      this.value = value;
+    }
+
+    public int getValue() {
+      return value;
+    }
+  }
 }
+
