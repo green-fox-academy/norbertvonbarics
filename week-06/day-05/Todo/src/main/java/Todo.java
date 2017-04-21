9b3e084b2d2b96d0034b627c4627a006b17d36e2
norbert barics <norbert.barics@gmail.com>
2017-04-21 12:35:41 +0200

    added todo object

---
+++ week-06/day-05/Todo/src/main/java/Todo.java
@@ -0,0 +1,11 @@
+
+public class Todo {
+
+  int id;
+  String todo;
+  
+  Todo(int id, String todo) {
+    this.id = id;
+    this.todo = todo;
+  }
+}
--- week-06/day-05/Todo/src/main/java/UnsupportedArgument.java
+++ week-06/day-05/Todo/src/main/java/UnsupportedArgument.java
@@ -5,8 +5,13 @@ class UnsupportedArgument {
   private static final String UNSUPPORTED = "Unsupported argument!\n";
 
   static void unsupported(String[] args) {
-    if (!args[0].equals("-c") || !args[0].equals("-a") || !args[0].equals("-l") || !args[0]
-        .equals("-la") || !args[0].equals("-r")) {
+    boolean argumentC = !args[0].equals("-c");
+    boolean argumentA = !args[0].equals("-a");
+    boolean argumentL = !args[0].equals("-l");
+    boolean argumentLA = !args[0].equals("-la");
+    boolean argumentR = !args[0].equals("-r");
+
+    if (argumentC || argumentA || argumentL || argumentLA || argumentR) {
       Ascii logo = new Ascii();
       System.out.println("\n" + LINE + UNSUPPORTED + LINE);
       logo.printUsage();
