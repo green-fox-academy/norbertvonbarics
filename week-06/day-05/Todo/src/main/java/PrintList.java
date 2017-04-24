0a91ea3032cee151c2ace77bf6f2db68ba02d097
norbert barics <norbert.barics@gmail.com>
2017-04-24 14:07:52 +0200

    added -l print method

--- week-06/day-05/Todo/src/main/java/PrintList.java
+++ week-06/day-05/Todo/src/main/java/PrintList.java
@@ -1,17 +1,28 @@
+import java.util.ArrayList;
 import java.util.List;
 
 class PrintList {
 
   static void printList(String[] args) {
     List<Todo> todoList = ReadFile.readFiles();
+    List<String> printedList = new ArrayList<>();
     if (args[0].equals("-la")) {
       for (int i = 0; i < todoList.size(); i++) {
         if (!todoList.get(i).checked) {
-          System.out.println((i + 1) + " [ ] #ID:" + todoList.get(i).id + " " + todoList.get(i).todo);
+          printedList.add((i + 1) + " [ ] #ID:" + todoList.get(i).id + " " + todoList.get(i).todo);
         } else {
-          System.out.println((i + 1) + " [X] #ID:" + todoList.get(i).id + " " + todoList.get(i).todo);
+          printedList.add((i + 1) + " [X] #ID:" + todoList.get(i).id + " " + todoList.get(i).todo);
         }
       }
+    } else if(args[0].equals("-l")) {
+      for (int j = 0; j < todoList.size(); j++) {
+        if (!todoList.get(j).checked) {
+          printedList.add((j + 1) + "[ ] #ID:" + todoList.get(j).id + " " + todoList.get(j).todo);
+        }
+      }
+    }
+    for (String lines : printedList) {
+      System.out.println(lines);
     }
   }
 }
